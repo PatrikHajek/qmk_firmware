0820b85a164eefc282264c6c0d8b588b02a53e27
PatrikHajek <hajan21@seznam.cz>
2025-01-12 11:18:02 +0100

    fix: conflicts with keybinds

--- keyboards/gmmk/numpad/keymaps/custom/keymap.c
+++ keyboards/gmmk/numpad/keymaps/custom/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     MO(1),   KC_PSLS,   KC_PAST,   KC_PMNS,
     KC_P7,   KC_P8,     KC_P9,     KC_PPLS,
-    KC_P4,   KC_P5,     KC_P6,     KC_F16,
-    KC_P1,   KC_P2,     KC_P3,     KC_0,
-    KC_F17,                         KC_F18
+    KC_P4,   KC_P5,     KC_P6,     HYPR(KC_0),
+    KC_P1,   KC_P2,     KC_P3,     HYPR(KC_3),
+    HYPR(KC_4),                    HYPR(KC_5)
   ),
   [1] = LAYOUT(
     _______,   KC_NUM,    KC_PAST,   KC_PMNS,
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(KC_F14, KC_F15)},
+    [0] = {ENCODER_CCW_CW(HYPR(KC_1), HYPR(KC_2))},
     [1] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 
