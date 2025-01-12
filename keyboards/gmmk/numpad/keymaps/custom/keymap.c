8a99fb0c154e7eff04700785a00a7382b1e889dd
PatrikHajek <hajan21@seznam.cz>
2025-01-12 01:08:36 +0100

    refactor: changed gmmk numpad keymap

--- keyboards/gmmk/numpad/keymaps/custom/keymap.c
+++ keyboards/gmmk/numpad/keymaps/custom/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(1),   KC_PSLS,   KC_PAST,   KC_PMNS,
     KC_P7,   KC_P8,     KC_P9,     KC_PPLS,
     KC_P4,   KC_P5,     KC_P6,     KC_F16,
-    KC_P1,   KC_P2,     KC_P3,     KC_PENT,
+    KC_P1,   KC_P2,     KC_P3,     KC_0,
     KC_F17,                         KC_F18
   ),
   [1] = LAYOUT(
