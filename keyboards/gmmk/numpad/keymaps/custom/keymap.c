69b76326b45145a4e07470e893f776f0735b7ff5
PatrikHajek <hajan21@seznam.cz>
2025-10-11 20:00:40 +0200

    refactor: change gmmk/numpad enter back to KC_ENTER

--- keyboards/gmmk/numpad/keymaps/custom/keymap.c
+++ keyboards/gmmk/numpad/keymaps/custom/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(1),   KC_PSLS,   KC_PAST,   KC_PMNS,
     KC_P7,   KC_P8,     KC_P9,     KC_PPLS,
     KC_P4,   KC_P5,     KC_P6,     LCAG(KC_0),
-    KC_P1,   KC_P2,     KC_P3,     LCAG(KC_3),
+    KC_P1,   KC_P2,     KC_P3,     KC_ENTER,
     LCAG(KC_4),                    LCAG(KC_5)
   ),
   [1] = LAYOUT(
